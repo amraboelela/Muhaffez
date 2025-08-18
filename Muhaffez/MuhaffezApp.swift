c9d17e1ba340bd352ef915892fb238bbcbf07bb6
Amr Aboelela <amraboelela@gmail.com>
2025-08-18 14:10:57 -0700

    initial files

---
+++ Muhaffez/MuhaffezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuhaffezApp.swift
+//  Muhaffez
+//
+//  Created by Amr Aboelela on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MuhaffezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuhaffezTests/MuhaffezTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuhaffezTests.swift
+//  MuhaffezTests
+//
+//  Created by Amr Aboelela on 8/18/25.
+//
+
+import Testing
+@testable import Muhaffez
+
+struct MuhaffezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
