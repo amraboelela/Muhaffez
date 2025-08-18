454f5c6a1cc2fc09f997107bf02ed8d717654b88
Amr Aboelela <amraboelela@gmail.com>
2025-08-18 15:04:36 -0700

    Added ArabicSpeechRecognizer

--- Muhaffez/ContentView.swift
+++ Muhaffez/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import Speech
+import AVFoundation
 
 struct ContentView: View {
+    @StateObject var recognizer = ArabicSpeechRecognizer()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text(recognizer.recognizedText)
+                .padding()
+                .border(Color.gray)
+
+            HStack {
+                Button("Start") {
+                    try? recognizer.startRecording()
+                }
+                Button("Stop") {
+                    recognizer.stopRecording()
+                }
+            }
         }
         .padding()
+        .onAppear {
+            SFSpeechRecognizer.requestAuthorization { status in
+                switch status {
+                case .authorized:
+                    print("Speech recognition authorized")
+                default:
+                    print("Speech recognition not authorized")
+                }
+            }
+        }
     }
 }
 
